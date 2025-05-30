3789c3a76e74bd64a51b97751844c78376183efa
huangdihd <hd20100104@163.com>
2025-05-30 20:04:13 +0800

    modified:   device.mk

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6765
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6765 \
     libgptutils \
     libz \
